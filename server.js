2667e6ace728ecdf2acebffa9f91e67192475604
Donghwa Lee <donghwalee328@gmail.com>
2015-10-14 05:06:01 -0400

    user login without encryption works at a basic level

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
+// variables
+
 var express           = require('express'),
+    router            = express.Router(),
     // PORT              = process.env.PORT || 3000,
     PORT              = 3000,
     server            = express(),
@@ -6,6 +9,7 @@ var express           = require('express'),
     MONGOURI          = "mongodb://localhost:27017",
     dbname            = "projecttwo",
     mongoose          = require('mongoose'),
+    Schema            = mongoose.Schema,
     ejs               = require('ejs'),
     morgan            = require('morgan'),
     methodOverride    = require('method-override'),
@@ -13,11 +17,34 @@ var express           = require('express'),
     expressEjsLayouts = require('express-ejs-layouts'),
     session           = require('express-session');
 
-var Topic            = mongoose.model("topic", {
+// schema version 1
+
+// var Topic = mongoose.model("topic", {
+//   userid: String,
+//   // name: String,
+//   title: String,
+//   description: { type: String, maxlength: 500 }
+// });
+
+// schema version 2
+
+var userSchema = Schema({
+  username: { type: String, required: true },
+  password: { type: String, required: true }
+});
+
+var User = mongoose.model("User", userSchema);
+
+
+var Topic = mongoose.model("topic", {
   userid: String,
   // name: String,
   title: String,
-  description: { type: String, maxlength: 500 }
+  description: { type: String, maxlength: 500 },
+  comments: [ String ],
+  likes: Number
+  // date created
+  // date last updated?
 });
 
 // var newTopic = new Topic({
@@ -34,6 +61,10 @@ var Topic            = mongoose.model("topic", {
 //     console.log(topic);
 //   }
 // })
+
+
+
+// server set and uses
 
 server.set('view engine', 'ejs');
 server.set('views', './views');
@@ -57,39 +88,74 @@ server.use(bodyParser.urlencoded({ extended: true}));
 server.use(function (req, res, next) {
    console.log("REQ DOT BODY", req.body);
    console.log("REQ DOT SESSION", req.session);
-
    next();
 });
 
-server.get('/super-secret-test', function (req, res) {
-  res.write("Welcome to my amazing app");
-  res.end();
-});
+
+// from the user creation:
+// var userController = require('./controllers/users.js');
+// server.use('/users', userController);
+
+
+
+// server.get('/super-secret-test', function (req, res) {
+//   res.write("Welcome to my amazing app");
+//   res.end();
+// });
 
 server.get('/', function (req, res) {
-  res.locals.userid = undefined;
+  res.locals.currentuser = undefined;
   res.render('login');
 });
 
 server.get('/login', function (req, res) {
+  res.locals.currentuser = undefined;
   res.render('login');
 })
 
 server.post('/logout', function (req, res) {
-  req.session.userId = undefined;
+  req.session.currentUser = undefined;
   res.redirect(302, '/');
-})
+});
+
+server.get('/register', function (req, res) {
+  res.locals.currentuser = undefined;
+  res.render('register');
+});
+
+server.post('/register', function (req, res) {
+  var newUser = User(req.body.user);
+
+  newUser.save(function (err, user) {
+    res.redirect(301, 'login');
+  });
+});
+
+server.get('/users/:id', function (req, res) {
+  User.findById(req.params.id, function (err, user) {
+    console.log(user);
+  });
+});
 
 server.post('/login', function (req, res) {
-  req.session.userId = req.body.userId;
-  res.redirect(302, '/topics')
+  // req.session.currentUser = req.body.currentUser;
+  // res.redirect(302, '/topics');
+  var attempt = req.body.user;
+  User.findOne({ username: attempt.username }, function (err, user) {
+    if (user && user.password === attempt.password) {
+      req.session.currentUser = user.username;
+      res.redirect(301, '/topics');
+    } else {
+      res.redirect(301, 'login');
+    }
+  });
 });
 
 server.use(function (req, res, next) {
-  if (req.session.userId == undefined) {
+  if (req.session.currentUser == undefined) {
     res.redirect(302, '/');
   } else {
-    res.locals.userid = req.session.userId;
+    res.locals.currentuser = req.session.currentUser;
     next();
   }
 });
@@ -108,9 +174,11 @@ server.get('/topics', function (req, res) {
 
 server.post('/topics', function (req, res) {
   var topic = new Topic({
-    userid: req.session.userId,
+    userid: req.session.currentUser,
     title: req.body.topic.title,
-    description: req.body.topic.description
+    description: req.body.topic.description,
+    comments: req.body.topic.comments.split(/,\s?/),
+    likes: req.body.topic.likes
   });
   topic.save(function (err, newTopic) {
     if (err) {
@@ -141,8 +209,6 @@ server.get('/topics/:id/edit', function (req, res) {
 
 server.patch('/topics/:id/', function (req, res) {
   var topicID = req.params.id;
-  var topicParams = req.body.topic;
-  var userid = req.session.userId;
 
   Topic.findOne({
     _id: topicID
@@ -180,8 +246,8 @@ server.get('/topics/new', function (req, res) {
   res.render('newtopic' );
 });
 
-server.get('/users/:userid', function (req, res) {
-  var particularUser = req.params.userid;
+server.get('/users/:id', function (req, res) {
+  var particularUser = req.params.id;
   Topic.find({
     userid: particularUser
   }, function (err, userTopics) {
@@ -194,6 +260,44 @@ server.get('/users/:userid', function (req, res) {
       });
     }
   });
+});
+
+server.get('/topics/:id', function (req, res) {
+  Topic.findById(req.params.id, function (err, particularTopic) {
+    if (err) {
+      console.log("darn...");
+    } else {
+      res.render('comments', {
+        topic: particularTopic
+      });
+    }
+  });
+});
+
+server.post('/topics/:id/newcomment', function (req, res) {
+  var topicID = req.params.id;
+  console.log("here");
+  Topic.findOne({
+    _id: topicID
+  }, function (err, foundTopic) {
+    if (err) {
+      res.write("something wrong w/ the ID...");
+      res.end();
+    } else {
+       res.render('newcomment', {
+          topic: foundTopic
+       });
+    }
+  });
+});
+
+
+
+// catchall as a last resort (needed?)
+
+server.use(function (req, res, next) {
+  res.send("You are done...");
+  res.end();
 });
 
 mongoose.connect(MONGOURI + "/" + dbname);
