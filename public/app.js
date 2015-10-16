944de4d5494b22a8867fbdf007d72385df4c43ea
Donghwa Lee <donghwalee328@gmail.com>
2015-10-16 03:01:10 -0400

    everything works for demo tomorrow

--- public/app.js
+++ public/app.js
@@ -7,3 +7,33 @@ $('.expand').on("click", function (e) {
 $('.collapse').on("click", function (e) {
   $('.hide-at-start').slideUp(500);
 });
+
+// hamburger menu animation code from Greg
+
+function slideMenu() {
+  var $hamburger = $('#hamburger-wrapper');
+  var $menuWrapper = $('#menu-wrapper');
+
+  $menuWrapper.toggleClass('open');
+  $hamburger.toggleClass('open');
+}
+
+function animateHamburger() {
+  var $hamburgerButton = $(this);
+  $hamburgerButton.toggleClass('open');
+}
+
+function setHamburgerListener(){
+var $hamburgerButton = $('#hamburger-button');
+$hamburgerButton.on('click', slideMenu);
+$hamburgerButton.on('click', animateHamburger);
+
+var $menu = $('#menu');
+var menuItems = $menu.children();
+
+for (var i = 0; i < menuItems.length; i++) {
+  $(menuItems[i]).on('click', selectMenuItem);
+}
+};
+
+setHamburgerListener();
